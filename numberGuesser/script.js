b18a7646825079d47e79d716d48bd60b44b97187
mp-web3 <mp.web3.t@gmail.com>
2023-03-08 23:21:59 +0100

    Let's go

--- numberGuesser/script.js
+++ numberGuesser/script.js
@@ -3,4 +3,30 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random()*10)
+};
 
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    if ((humanGuess >= 0) && (humanGuess <= 9 )) {
+        if (Math.abs(humanGuess - targetGuess) < Math.abs(computerGuess - targetGuess)) {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        alert('number out of range');
+    }
+};
+
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore ++;
+    } else {
+        computerScore ++;
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber ++;
+}
