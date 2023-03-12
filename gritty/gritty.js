5350a5dddf684309c6ccdeafa06373c249125972
mp-web3 <mp.web3.t@gmail.com>
2023-03-12 19:30:38 +0100

    played around with event listeners

--- gritty/gritty.js
+++ gritty/gritty.js
@@ -1,1 +1,23 @@
-alert('Get ready for Javascript')
+
+
+let banner = document.getElementById('banner-1');
+let buttonColor = document.getElementById('original-color');
+
+function colorValue() {
+    return Math.floor(Math.random() * 256);
+}
+
+function changeBackgroudColorRandom(event) {
+    if (event.target === banner) {
+        let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
+    event.target.style.backgroundColor = randomColor;
+    }
+}
+
+banner.onclick = changeBackgroudColorRandom;
+
+function changeColorToOriginal() {
+    banner.style.backgroundColor = '#4D00FF';
+}
+
+buttonColor.onclick = changeColorToOriginal;
